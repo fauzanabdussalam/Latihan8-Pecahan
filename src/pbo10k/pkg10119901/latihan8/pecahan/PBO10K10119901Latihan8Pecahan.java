bcc504d8ab12205af3781a003b809e576a0de86f
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 23:03:02 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan8/pecahan/PBO10K10119901Latihan8Pecahan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Program Pecahan
+ *
+ */
+public class PBO10K10119901Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float \t : " + f);
+        System.out.println("Double \t : " + d);
+    }
+    
+}
